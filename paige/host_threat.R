c1e2de344ff1a14a74b97f1ed31cc7134457b929
gokhale <deven.gokhale@uga.edu>
2019-04-24 11:37:30 -0400

    changes in the app
    made the required changes for faceting options .
    
    made the required changes for the slidebars.
    
    made the required changes to the plotting theme and styles

---
+++ paige/host_threat.R
@@ -0,0 +1,134 @@
+# The simplest bargraph 
+library(shiny)
+library(tidyverse)
+library(magrittr)
+library(plotly)
+setwd("~/Desktop/shiny_IDEAS_capstone-master/paige")
+################################################################################################# 
+######## with continuous variable side bars #####################################################
+#################################################################################################
+app <- shinyApp(
+  ui = fluidPage(
+
+    sliderInput(inputId = "masskg",
+                label = "Mammal Mass (Kg):",
+                value = c(0.5, 1000), min = 0.5, max = 1000),
+
+    checkboxGroupInput(inputId = "mamOrd",
+                       label = "Mammal order:",
+                       choices = c("Carnivores" = "carnivores",
+                                   "Ungulates" = "ungulates"),
+                       selected = c("carnivores", "ungulates")),
+
+    checkboxGroupInput(inputId = "contin",
+                       label = "Continent:",
+                       choices = c("Africa" = "Africa",
+                                   "Asia" = "Asia",
+                                   "Australia" = "Australia",
+                                   "Europe" = "Europe",
+                                   "North America" = "North America"),
+                       selected = c("Africa", "Asia", "Australia", "Europe", "North America")),
+    
+    
+    radioButtons(inputId = "compare", 
+                 label = "Compare distributions:", 
+                 choices = c("No", "Continent", "Group"), 
+                 selected = c("No")), 
+    
+    
+    selectInput(inputId = "grp",
+                label = "Grouping variable:",
+                choices = c("Family" = "HostFamily",
+                            "Environment" = "HostEnvironment")),
+    
+    sliderInput(inputId = "homerange",
+                label = "Mammal home range (km sqrd.)",
+                value = c(0.02, 77500), min = 0.02, max = 77500),
+
+    plotlyOutput("plot")
+  ),
+  server = function(input, output) {
+    mammals <- read.csv("distinct-hosts.csv")
+
+    dat <- reactive({
+
+      if(input$compare == "No") {
+        foo <- mammals %>%
+          filter(Group %in% input$mamOrd) %>%
+          filter(continent %in% input$contin) %>%
+          filter(hostHomeRange >= input$homerange[1] & hostHomeRange <= input$homerange[2]) %>%
+          filter(massKG >= input$masskg[1] & massKG <= input$masskg[2]) %>%
+          # unclear why this .dots arg is neccessary but it is...
+          group_by(.dots=input$grp, type) %>%
+          tally() %>% data.frame()
+        
+       } else if (input$compare == "Continent") {
+        foo <- mammals %>%
+          filter(Group %in% input$mamOrd) %>%
+          filter(continent %in% input$contin) %>%
+          filter(hostHomeRange >= input$homerange[1] & hostHomeRange <= input$homerange[2]) %>%
+          filter(massKG >= input$masskg[1] & massKG <= input$masskg[2]) %>%
+          # unclear why this .dots arg is neccessary but it is...
+          group_by(.dots=input$grp, continent, type) %>%
+          tally() %>% data.frame()
+       } else {
+         foo <- mammals %>%
+           filter(Group %in% input$mamOrd) %>%
+           filter(continent %in% input$contin) %>%
+           filter(hostHomeRange >= input$homerange[1] & hostHomeRange <= input$homerange[2]) %>%
+           filter(massKG >= input$masskg[1] & massKG <= input$masskg[2]) %>%
+           # unclear why this .dots arg is neccessary but it is...
+           group_by(.dots=input$grp, Group, type) %>%
+           tally() %>% data.frame()
+      }
+      
+      
+      
+      
+
+      return(foo)
+    })
+
+    output$plot <- renderPlotly(
+      # needs to be evaluated as a string
+      if(input$compare == "No") { 
+      ggplot(dat(), aes_string(fill = "type", y = "n", x = input$grp)) +
+        geom_bar(stat = "identity", color = "black") + 
+        labs(x="", y="", fill="")  +
+        theme_void() + 
+        theme(legend.text=element_text(size=rel(1.5)),
+              strip.text = element_text(size=18), 
+              axis.text.x = element_text(angle = 90, hjust = 1, size = 10), 
+              panel.spacing = unit(2, "lines"))
+      } else if (input$compare == "Continent") {
+        ggplot(dat(), aes_string(fill = "type", y = "n", x = input$grp)) +
+          geom_bar(stat = "identity", color = "black") + 
+          labs(x="", y="", fill="")  +
+          facet_wrap(~continent) +
+          theme_void() + 
+          theme(legend.text=element_text(size=rel(1.5)),
+                strip.text = element_text(size=18), 
+                axis.text.x = element_text(angle = 90, hjust = 1, size = 10), 
+                panel.spacing = unit(2, "lines"))
+      } else {
+        ggplot(dat(), aes_string(fill = "type", y = "n", x = input$grp)) +
+          geom_bar(stat = "identity", color = "black") + 
+          labs(x="", y="", fill="")  +
+          facet_wrap(~Group) +
+          theme_void() + 
+          theme(legend.text=element_text(size=rel(1.5)),
+                strip.text = element_text(size=18), 
+                axis.text.x = element_text(angle = 90, hjust = 1, size = 10), 
+                panel.spacing = unit(2, "lines"))
+      }
+        
+       
+        
+
+    )
+  }
+)
+
+runApp(app)
+
+
