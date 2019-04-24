2f4ac4c65c7863131401c61a72e36439005135d7
gokhale <deven.gokhale@uga.edu>
2019-04-24 13:20:28 -0400

    changes with slider scales
    contains changes with the slider scale

--- paige/host_threat.R
+++ paige/host_threat.R
@@ -39,12 +39,12 @@ app <- shinyApp(
                             "Environment" = "HostEnvironment")),
     
     sliderInput(inputId = "homerange",
-                label = "Mammal home range (km sqrd.)",
-                value = c(0.02, 77500), min = 0.02, max = 77500),
+                label = "Mammal home range (km sqrd.; log scale)",
+                value = c(log(0.02), log(77500)), min = floor(log(0.02)), max = ceiling(log(77500))),
     
     sliderInput(inputId = "masskg",
                 label = "Mammal Mass (Kg):",
-                value = c(0.5, 1000), min = 0.5, max = 1000),
+                value = c(0.5, 1000), min =0.5, max = 1000),
 
     plotlyOutput("plot")
   ),
@@ -57,8 +57,8 @@ app <- shinyApp(
         foo <- mammals %>%
           filter(Group %in% input$mamOrd) %>%
           filter(continent %in% input$contin) %>%
-          filter(hostHomeRange >= input$homerange[1] & hostHomeRange <= input$homerange[2]) %>%
-          filter(massKG >= input$masskg[1] & massKG <= input$masskg[2]) %>%
+          filter(hostHomeRange >= exp(input$homerange[1]) & hostHomeRange <= exp(input$homerange[2])) %>%
+          filter(massKG >= input$masskg[1] & massKG <= exp(input$masskg[2])) %>%
           # unclear why this .dots arg is neccessary but it is...
           group_by(.dots=input$grp, type) %>%
           tally() %>% data.frame()
@@ -67,7 +67,7 @@ app <- shinyApp(
         foo <- mammals %>%
           filter(Group %in% input$mamOrd) %>%
           filter(continent %in% input$contin) %>%
-          filter(hostHomeRange >= input$homerange[1] & hostHomeRange <= input$homerange[2]) %>%
+          filter(hostHomeRange >= exp(input$homerange[1]) & hostHomeRange <= exp(input$homerange[2])) %>%
           filter(massKG >= input$masskg[1] & massKG <= input$masskg[2]) %>%
           # unclear why this .dots arg is neccessary but it is...
           group_by(.dots=input$grp, continent, type) %>%
@@ -76,7 +76,7 @@ app <- shinyApp(
          foo <- mammals %>%
            filter(Group %in% input$mamOrd) %>%
            filter(continent %in% input$contin) %>%
-           filter(hostHomeRange >= input$homerange[1] & hostHomeRange <= input$homerange[2]) %>%
+           filter(hostHomeRange >= exp(input$homerange[1]) & hostHomeRange <= exp(input$homerange[2])) %>%
            filter(massKG >= input$masskg[1] & massKG <= input$masskg[2]) %>%
            # unclear why this .dots arg is neccessary but it is...
            group_by(.dots=input$grp, Group, type) %>%
@@ -100,7 +100,7 @@ app <- shinyApp(
         theme(legend.text=element_text(size=rel(1.5)),
               strip.text = element_text(size=18), 
               axis.text.x = element_text(angle = 90, hjust = 1, size = 10), 
-              panel.spacing = unit(2, "lines"))
+              panel.spacing = unit(1.5, "lines"))
       } else if (input$compare == "Continent") {
         ggplot(dat(), aes_string(fill = "type", y = "n", x = input$grp)) +
           geom_bar(stat = "identity", color = "black") + 
@@ -110,7 +110,7 @@ app <- shinyApp(
           theme(legend.text=element_text(size=rel(1.5)),
                 strip.text = element_text(size=18), 
                 axis.text.x = element_text(angle = 90, hjust = 1, size = 10), 
-                panel.spacing = unit(2, "lines"))
+                panel.spacing = unit(1.75, "lines"))
       } else {
         ggplot(dat(), aes_string(fill = "type", y = "n", x = input$grp)) +
           geom_bar(stat = "identity", color = "black") + 
@@ -119,8 +119,8 @@ app <- shinyApp(
           theme_void() + 
           theme(legend.text=element_text(size=rel(1.5)),
                 strip.text = element_text(size=18), 
-                axis.text.x = element_text(angle = 45, hjust = 1, size = 10), 
-                panel.spacing = unit(2, "lines"))
+                axis.text.x = element_text(angle = 90, hjust = 1, size = 10), 
+                panel.spacing = unit(1.75, "lines"))
       }
         
        
