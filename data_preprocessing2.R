e8344e3ce7472c685d2665fba8ad4953cd9f4575
Ellen Cheng <ellencheng@Ellens-MacBook-Pro.local>
2019-04-25 06:34:58 -0400

    Almost final--added tables, maps, and parasite pies

--- data_preprocessing2.R
+++ data_preprocessing2.R
@@ -1,19 +1,28 @@
-### SCRIPT TO ADD MORE INFORMATION TO DATA FILE ###
+# Script to add common names & IUCN URL to host dataset
 
-# setwd("~/Desktop/GMPD2") # Local location of files
 
-### LIBRARIES ----
-library(tidyverse)
-library(readxl)
+library(taxize)
 
-### PROCESS DATA ----
+mammals = read.csv("distinct-hosts.csv")
+mammals$commonName <- lapply(mammals$HostCorrectedName, FUN = function(x) unlist(sci2comm(x)[[1]]))
+mammals$commonName <- unlist(as.character(x[["commonName"]]))
 
-# Add host order and family, then resave data as 'shiny_data2.csv' 
-threat_data <- read_excel(path = "data/ThreatPSRSoniaAmy2005.xlsx", sheet = sheet_names1[1]) %>%
-  select(order = Order, hostFamily = 'Host family', HostCorrectedName = 'MSW Binomial') %>% # grab host order and family
-  distinct()
-dat <- read_csv("data/shiny_data.csv") %>%
-  left_join(threat_data, by = "HostCorrectedName") %>%
-  distinct() # keep unique records
-write_csv(x = dat, path = "data/shiny_data2.csv") # save data as 'shiny_data2
+# Fix the four that couldn't be identified
+mammals$commonName[mammals$HostCorrectedName=="Arctocephalus pusillus"] <- "brown fur seal"
+mammals$commonName[mammals$HostCorrectedName=="Caracal caracal"] <- "caracal"
+mammals$commonName[mammals$HostCorrectedName=="Equus burchellii"] <- "plains zebra"
+mammals$commonName[mammals$HostCorrectedName=="Damaliscus pygargus"] <- "bontebok"
+
+mammals$status <- factor(mammals$type, levels = c("NonThreatened", "Disease", "ThreatOther", "ThreatDisease"), labels = c("NT - Other", "NT - Disease", "T - Other", "T - Disease"))
+
+mammals$IUCNlink <- NA
+for (i in 1:nrow(mammals)) {
+  url <- paste0("https://www.iucnredlist.org/search?query=", unlist(strsplit(as.character(mammals[i, "HostCorrectedName"]), split= " ")[[1]][1]),"%20",unlist(strsplit(as.character(mammals[i, "HostCorrectedName"]), split= " ")[[1]][2]),"&searchType=species")
+  mammals[i, "IUCNlink"] <- paste0("<a href='", url,"'>", url,"</a>")
+  
+}
+
+mammals %<>% mutate(
+  poptext = paste0("Scientific Name:  ", HostCorrectedName, "</br>Common Name:  ", commonName, "</br>Threat Status:  ", status, "</br>Country:  ", country))
+write.csv(mammals, "distinct-hosts-mod.csv", row.names = FALSE)
 
